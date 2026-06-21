0916ecd2603e88814bf90382b3670513252abe9e
あなたの名前 <>
2026-06-21 16:14:16 +0900

    第3回 継承・抽出課題

--- src/character.ts
+++ src/character.ts
@@ -1,7 +1,7 @@
 export abstract class Character {
   protected name: string; // インスタンスからはアクセス出来ないが、継承先からはアクセスできる
-  private hp: number; // インスタンスからも継承先からもアクセス出来ない
-
+  protected hp: number; // インスタンスからも継承先からもアクセス出来ない
+//privateだと継承先のEnemyからhpを参照できないため、をprotectedに変更。
   constructor(name: string, hp: number) {
     this.name = name;
     this.hp = hp;
