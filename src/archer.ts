484eb5e3018d01c80335f627117cef10054f2802
あなたの名前 <>
2026-06-28 20:40:46 +0900

    Implement opponent attack system

--- src/archer.ts
+++ src/archer.ts
@@ -4,16 +4,19 @@ export class Archer extends Character {
   private arrows: number;
 
   constructor(name: string, hp: number, arrows: number) {
-    super(name, hp);
+    super(name, hp, 30);
     this.arrows = arrows;
-  };
+  }
 
-  attack() {
+  override attack(opponent:Character) {
     if (this.arrows <= 0) {
       console.log(`${this.name} は矢がない!`);
       return;
     }
     this.arrows--;
     console.log(`${this.name} は矢を放った!`);
+
+    opponent.takeDamage(30);
+    // opponent.takeDamage(this.power);
   }
 }
