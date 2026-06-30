4f4a7917dbc260818deb463fa6f09cf7ca1f9d4c
あなたの名前 <>
2026-06-30 21:13:27 +0900

    mageにconstructorを

--- src/mage.ts
+++ src/mage.ts
@@ -1,9 +1,13 @@
-import { Character } from './character.ts';
+import { Character } from "./character.ts";
 
 export class Mage extends Character {
-  override attack(opponent:Character) {
+  constructor(name: string, hp: number) {
+    super(name, hp, 30);
+  }
+  override attack(opponent: Character) {
     console.log(`${this.name}は魔法を唱えた！`);
 
-    opponent.takeDamage(30);
+    // opponent.takeDamage(30);
+    opponent.takeDamage(this.power);
   }
 }
